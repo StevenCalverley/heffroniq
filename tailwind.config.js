9e7ae3e7113259d21aa6f8db938a6e2ba61b1e83
Steve Calverley <steve.calverley@heffron.com.au>
2021-02-19 18:44:41 +1100

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+const { teal } = require('tailwindcss/colors')
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'light-blue': colors.lightBlue,
+        teal: colors.teal,
+        cyan: colors.cyan,
+        rose: colors.rose,
+        'heffron-teal': {
+          light: '#37BEBD',
+          dark: '#1E7D97',
+        },
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+  ],
+}
