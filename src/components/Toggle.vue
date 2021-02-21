6a6b09c007909a5fe7edcdc193b47016af105e23
Steve Calverley <steve.calverley@heffron.com.au>
2021-02-21 21:50:45 +1100

    Add Toggle

---
+++ src/components/Toggle.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- Enabled: "bg-teal-500", Not Enabled: "bg-gray-200" -->
+  <button
+    type="button"
+    @click="isOn = !isOn"
+    :class=" isOn ? 'bg-teal-500' : 'bg-gray-200'"
+    class="ml-4 relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-light-blue-500"
+    aria-pressed="true"
+    :aria-labelledby="aria.labelledby"
+    :aria-describedby="aria.describedby">
+    <span class="sr-only">Use setting</span>
+    <!-- Enabled: "translate-x-5", Not Enabled: "translate-x-0" -->
+    <span
+      aria-hidden="true"
+      :class="isOn ? 'translate-x-5' : 'translate-x-0'"
+      class="inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"
+    ></span>
+  </button>
+</template>
+
+<script>
+  export default {
+    props: {
+      aria: {
+        type: Object,
+      },
+    },
+    data: () => ({
+      isOn: false,
+    })
+  }
+</script>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -181,12 +181,7 @@
                         Nulla amet tempus sit accumsan. Aliquet turpis sed sit lacinia.
                       </p>
                     </div>
-                    <!-- Enabled: "bg-teal-500", Not Enabled: "bg-gray-200" -->
-                    <button type="button" class="bg-gray-200 ml-4 relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-light-blue-500" aria-pressed="true" aria-labelledby="privacy-option-1-label" aria-describedby="privacy-option-1-description">
-                      <span class="sr-only">Use setting</span>
-                      <!-- Enabled: "translate-x-5", Not Enabled: "translate-x-0" -->
-                      <span aria-hidden="true" class="translate-x-0 inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"></span>
-                    </button>
+                    <toggle :aria="{ labelledby: 'privacy-option-1-label', describedby: 'privacy-option-1-description' }" />
                   </li>
                   <li class="py-4 flex items-center justify-between">
                     <div class="flex flex-col">
@@ -197,12 +192,7 @@
                         Pharetra morbi dui mi mattis tellus sollicitudin cursus pharetra.
                       </p>
                     </div>
-                    <!-- Enabled: "bg-teal-500", Not Enabled: "bg-gray-200" -->
-                    <button type="button" class="bg-gray-200 ml-4 relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-light-blue-500" aria-pressed="false" aria-labelledby="privacy-option-2-label" aria-describedby="privacy-option-2-description">
-                      <span class="sr-only">Use setting</span>
-                      <!-- Enabled: "translate-x-5", Not Enabled: "translate-x-0" -->
-                      <span aria-hidden="true" class="translate-x-0 inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"></span>
-                    </button>
+                    <toggle :aria="{ labelledby: 'privacy-option-2-label', describedby: 'privacy-option-2-description' }" />
                   </li>
                   <li class="py-4 flex items-center justify-between">
                     <div class="flex flex-col">
@@ -213,12 +203,7 @@
                         Integer amet, nunc hendrerit adipiscing nam. Elementum ame
                       </p>
                     </div>
-                    <!-- Enabled: "bg-teal-500", Not Enabled: "bg-gray-200" -->
-                    <button type="button" class="bg-gray-200 ml-4 relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-light-blue-500" aria-pressed="true" aria-labelledby="privacy-option-3-label" aria-describedby="privacy-option-3-description">
-                      <span class="sr-only">Use setting</span>
-                      <!-- Enabled: "translate-x-5", Not Enabled: "translate-x-0" -->
-                      <span aria-hidden="true" class="translate-x-0 inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"></span>
-                    </button>
+                    <toggle :aria="{ labelledby: 'privacy-option-3-label', describedby: 'privacy-option-3-description' }" />
                   </li>
                   <li class="py-4 flex items-center justify-between">
                     <div class="flex flex-col">
@@ -229,12 +214,7 @@
                         Adipiscing est venenatis enim molestie commodo eu gravid
                       </p>
                     </div>
-                    <!-- Enabled: "bg-teal-500", Not Enabled: "bg-gray-200" -->
-                    <button type="button" class="bg-gray-200 ml-4 relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-light-blue-500" aria-pressed="true" aria-labelledby="privacy-option-4-label" aria-describedby="privacy-option-4-description">
-                      <span class="sr-only">Use setting</span>
-                      <!-- Enabled: "translate-x-5", Not Enabled: "translate-x-0" -->
-                      <span aria-hidden="true" class="translate-x-0 inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"></span>
-                    </button>
+                    <toggle :aria="{ labelledby: 'privacy-option-4-label', describedby: 'privacy-option-4-description' }" />
                   </li>
                 </ul>
               </div>
@@ -254,8 +234,8 @@
   </main>
 </template>
 
-<script>
-
+<script setup>
+import Toggle from '../components/Toggle.vue'
 </script>
 
 <style>
